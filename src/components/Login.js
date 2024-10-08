eaf9c142d017231352c4001a1aa67d19f67ce6c4
Aviral Singh Chauhan <blr.aviral@gmail.com>
2024-10-08 19:41:47 +0530

    some other resp changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -95,7 +95,7 @@ const Login = () => {
     <div className='bg-[url("https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_large.jpg")] min-h-screen min-w-screen relative'>
       <div className='bg-black/50 min-h-screen h-full w-full flex items-center justify-center'>
         <Header />
-        <form onSubmit={(e) => handleFormSubmit(e)} className='p-14 bg-black/70 text-white flex flex-col items-start justify-center gap-4 w-[min(90%,450px)]'>
+        <form onSubmit={(e) => handleFormSubmit(e)} className='sm:p-14 p-8 bg-black/70 text-white flex flex-col items-start justify-center gap-4 w-[min(90%,450px)]'>
           <h1 className='text-4xl font-extrabold mb-5'>{isSignIn ? "Sign In" : "Sign Up"}</h1>
           {
             !isSignIn && <>
